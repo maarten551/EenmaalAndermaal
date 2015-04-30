cc8b05903e899d4083f368b96d8362702ee81cf0
daymiandejong <daymian.dejong@hotmail.com>
2015-04-30 14:58:34 +0200

    headertjeeee nog een keer

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
         <h3> er is een verandering aangemaakt</h3>
         <h1>Maarten stinkt maar is best wel cool</h1>
         <h1>Daymian stinkt niet en is cool</h1>
+        <h1>Daymian stinkt niet en is cooler dan maarten</h1>
     </body>
 </html>
