1c3869b977ff56d9ddfc161ee2f93ff40e38fe02
Maarten Bobbeldijk <maarten12333@hotmail.com>
2015-04-30 14:39:37 +0200

    conflict example

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <h1>Maarten stinkt.</h1>
         <h3> er is een verandering aangemaakt</h3>
         <h1>Maarten stinkt maar is best wel cool</h1>
-        <h1>Daymian stinkt niet en is cool</h1>
+        <h1>Daymian is een piemelbijter, jwz</h1>
     </body>
 </html>
