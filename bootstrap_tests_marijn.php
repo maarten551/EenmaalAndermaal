c161b823e2b874ffee3ce32a32be51213b639b9a
Marijn <marijn.martin@hotmail.com>
2015-05-01 09:41:34 +0200

    bootstrap css en js toegevoegd

---
+++ bootstrap_tests_marijn.php
@@ -0,0 +1,14 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Marijn
+ * Date: 1-5-2015
+ * Time: 09:28
+ */
+
+<html>
+    <body>
+        <<link rel="stylesheet" type="text/css" href="">
+    </body>
+
+</html>
