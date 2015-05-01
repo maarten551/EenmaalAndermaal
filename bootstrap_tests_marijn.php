48819673d3bb2a1e1a1921381bca126dea053ce3
Marijn <marijn.martin@hotmail.com>
2015-05-01 14:07:21 +0200

    navbar gemaakt en verder geexpirimenteerd

--- bootstrap_tests_marijn.php
+++ bootstrap_tests_marijn.php
@@ -27,67 +27,47 @@
     </head>
 
     <body>
+
+    <!-- de navbar wordt gemaakt -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"> <img src="images/logo2.png" alt="Eenmaal Andermaal" width="50" height="50"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Geavanceerd zoeken</a></li>
+                    <li class="divider-vertical"></li>
+                    <li><a href="#">Inloggen</a></li>
+                    <li><a href="#">Aanmelden</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
         <!-- aanmaken van witte contentvak -->
         <div class="contentvak">
 
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Brand</a>
-                    </div>
-
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                            <li><a href="#">Link</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">  <span class="glyphicon glyphicon-menu-hamburger"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form class="navbar-form navbar-left" role="search">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#">Link</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
-
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-5ths" style = "background-color: red"> start</div>
-                <div class="col-xs-12 col-sm-6 col-md-5ths" style = "background-color: green">start2</div>
-                <div class="col-xs-12 col-sm-6 col-md-5ths" style = "background-color: blue">start3</div>
+                <div class="col-xs-12 col-sm-12 col-md-5ths" style= "background-color: red"   >start </div>
+                <div class="col-xs-12 col-sm-6 col-md-5ths" style = "background-color: green" >start2</div>
+                <div class="col-xs-12 col-sm-6 col-md-5ths" style = "background-color: blue"  >start3</div>
                 <div class="col-xs-12 col-sm-6 col-md-5ths" style = "background-color: yellow">start4</div>
                 <div class="col-xs-12 col-sm-6 col-md-5ths" style = "background-color: orange">start5</div>
             </div>
