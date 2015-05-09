4e2d214f89eb5156362d388b1682100ef034003a
Maarten Bobbeldijk <maarten12333@hotmail.com>
2015-05-09 22:51:22 +0200

    Database example added

--- PDO-test/index.php
+++ PDO-test/index.php
@@ -6,14 +6,41 @@
 include 'MSSQLSettings.php';
 
 try {
-    $conn = new PDO( "sqlsrv:server=".MSSQLSettings::$databaseHost.";Database=".MSSQLSettings::$databaseName, MSSQLSettings::$username, MSSQLSettings::$password);
+    $connParameters = array(
+        "Database" => MSSQLSettings::$databaseName,
+        "UID" => MSSQLSettings::$username,
+        "PWD" => MSSQLSettings::$password
+    );
+
+    $conn = sqlsrv_connect(MSSQLSettings::$databaseHost, $connParameters);
+
+    if($conn) {
+        echo "Success";
+    } else {
+        echo "Connection could not be established.<br />";
+        die( print_r( sqlsrv_errors(), true));
+    }
+
+    $query = "SELECT * FROM [dbo].[User] WHERE username != ?";
+    $stmt = sqlsrv_prepare($conn, $query, array("testUser3"));
+
+    if(!sqlsrv_execute($stmt))
+        die( print_r( sqlsrv_errors()[0]["message"], true));
+
+    if(sqlsrv_has_rows($stmt)) {
+        while($row = sqlsrv_fetch_object($stmt)) {
+            var_dump($row);
+        }
+    }
+
+    /*$conn = new PDO( "sqlsrv:server=".MSSQLSettings::$databaseHost.";Database=".MSSQLSettings::$databaseName, MSSQLSettings::$username, MSSQLSettings::$password);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     $statement = $conn->prepare("SELECT * FROM USERS WHERE SEX = :SEX");
     $statement->execute(array(":SEX" => "M"));
     while($row = $statement->fetch(PDO::FETCH_OBJ)) {
         echo "<h2>".$row->USERNAME."</h2>";
-    }
-} catch( PDOException $e ) {
+    }*/
+} catch(Exception $e) {
     die($e->getMessage());
 }
 
