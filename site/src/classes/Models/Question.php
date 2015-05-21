d2eea7a56f48072edf7371c50d252a1d409af251
Maarten Bobbeldijk <maarten12333@hotmail.com>
2015-05-21 13:58:35 +0200

    Weird bug with renaming directory and only changing a letter to uppercase

---
+++ site/src/classes/Models/Question.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace src\classes\Models;
+
+
+use src\classes\DatabaseHelper;
+
+class Question extends Model {
+    protected $id;
+    protected $questionText;
+
+    public function __construct(DatabaseHelper $databaseHelper, $primaryKeyValue = null) {
+        parent::__construct($databaseHelper);
+        $this->tableName = "Question";
+        $this->primaryKeyName = "id";
+        $this->hasIdentity = true;
+        $this->databaseFields["required"]["questionText"] = "quote";
+
+        $this->setId($primaryKeyValue);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuestionText()
+    {
+        return $this->get("questionText");
+    }
+
+    /**
+     * @param mixed $questionText
+     */
+    public function setQuestionText($questionText)
+    {
+        $this->set("questionText", $questionText);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    private function setId($id) {
+        $this->id = $id;
+    }
+}
