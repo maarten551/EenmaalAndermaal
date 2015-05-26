aaa31fcbf30ab65dc9b5cd351f21040ec12d40bf
Marijn <marijn.martin@hotmail.com>
2015-05-26 11:19:49 +0200

    commit voor maarten

--- site/src/classes/Pages/home.php
+++ site/src/classes/Pages/home.php
@@ -14,12 +14,19 @@ class Home extends AbstractPage {
     protected function createHTML() {
         $content = new HTMLParameter($this->HTMLBuilder, "content\\content-homepage.html");
         $product = new HTMLParameter($this->HTMLBuilder, "product\\product-item.html");
+        $mobileMenu= new HTMLParameter($this->HTMLBuilder, "product\\mobile-category.html");
+        $desktopMenu= new HTMLParameter($this->HTMLBuilder, "product\\desktop-category.html");
+        $category = new HTMLParameter($this->HTMLBuilder, "product\\category.html");
         $testString = array("Deze beschrijving is te lang en moet afgekort worden", "korte beschrijving");
         $randomElement = $testString[array_rand($testString, 1)];
+
 
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("content", $content);
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("product", $product);
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByString("titel", $randomElement);
+        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("mobile-category", $mobileMenu);
+        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("desktop-category", $desktopMenu);
+        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("category", $category);
         echo $this->HTMLBuilder->getHTML();
     }
 }
