ca02efeb43c1bdd9ef9ebd2c2b7c22c97faa5e7c
Marijn <marijn.martin@hotmail.com>
2015-05-28 09:50:56 +0200

    -nieuw directory omdat oude raar deed

--- site/index.php
+++ site/index.php
@@ -2,6 +2,7 @@
 use src\classes\DatabaseHelper;
 use src\classes\HTMLBuilder;
 use src\classes\Models\User;
+use src\classes\PageController;
 use src\classes\UserHelper;
 
 require_once "src/libraries/password.php"; //For password hashing functionality for PHP < 5.5, server is 5.4.35, source: https://github.com/ircmaxell/password_compat
@@ -12,23 +13,46 @@ function __autoload($class_name) { //PHP will use this function if a class file 
 
 session_start();
 date_default_timezone_set("Europe/Amsterdam");
-
-$databaseHelper = new DatabaseHelper();
-$userHelper = new UserHelper($databaseHelper);
-//$user = $userHelper->loginUser("18ribs", "Pzch8KjgmMLXjsBr", $databaseHelper);
-$user = $userHelper->getLoggedInUser();
-echo $_SESSION['loggedInUsername'];
-if($user !== null) {
-	$user->getBirthdate();
-	var_dump($user);
-} else {
-	echo "Login failed";
-}
+//
+//$databaseHelper = new DatabaseHelper();
+//$userHelper = new UserHelper($databaseHelper);
+////$user = $userHelper->loginUser("18ribs", "Pzch8KjgmMLXjsBr", $databaseHelper);
+//$user = $userHelper->getLoggedInUser();
+//echo $_SESSION['loggedInUsername'];
+//if($user !== null) {
+//	$user->getBirthdate();
+//	var_dump($user);
+//} else {
+//	echo "Login failed";
+//}
 
 //    $arguments = (isset($_GET['arg'])) ? $_GET['arg'] : "index";
 //    $PageController = new PageController($arguments);
 
-//$arguments = (isset($_GET['arg'])) ? $_GET['arg'] : "home";
-//$PageController = new PageController($arguments);
+//$databaseHelper = new DatabaseHelper();
+//$rubric = new \src\classes\Models\Rubric($databaseHelper, 1);
+//$children = $rubric->getChildren();
+//$parentId = array();
+//$children2 = array();
+//$childrenId = array();
+//
+//foreach($children as $child){
+//    $parentId[] = $child->getId();
+//    $children2[] = $child->getName();
+//    $childrenId[] = $child->getId();
+//}
+//
+//for($i = 2; $i<sizeof($childrenId); $i++){
+//    $temprubric = new \src\classes\Models\Rubric($databaseHelper, $i);
+//    $tempchildren = $temprubric->getChildren();
+//    foreach($tempchildren as $temp) {
+//        $a = $temp->getName();
+//        echo $a . ". parent id =";
+//        echo $parentId[$i] . "</br>";
+//    }
+//}
+//
+$arguments = (isset($_GET['arg'])) ? $_GET['arg'] : "home";
+$PageController = new PageController($arguments);
 
 $databaseHelper->closeConnection();
--- site/src/classes/Pages/home.php
+++ site/src/classes/Pages/home.php
@@ -11,22 +11,54 @@ class Home extends AbstractPage {
         $this->createHTML();
     }
 
+
     protected function createHTML() {
         $content = new HTMLParameter($this->HTMLBuilder, "content\\content-homepage.html");
-        $product = new HTMLParameter($this->HTMLBuilder, "product\\product-item.html");
-        $mobileMenu= new HTMLParameter($this->HTMLBuilder, "product\\mobile-category.html");
-        $desktopMenu= new HTMLParameter($this->HTMLBuilder, "product\\desktop-category.html");
-        $category = new HTMLParameter($this->HTMLBuilder, "product\\category.html");
-        $testString = array("Deze beschrijving is te lang en moet afgekort worden", "korte beschrijving");
-        $randomElement = $testString[array_rand($testString, 1)];
-
-
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("content", $content);
-        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("product", $product);
-        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByString("titel", $randomElement);
-        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("mobile-category", $mobileMenu);
-        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("desktop-category", $desktopMenu);
-        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("category", $category);
+
+        $HTMLCategoryDesktop = new HTMLParameter($this->HTMLBuilder, "content\\desktop-category.html");
+        $HTMLCategoryMobile = new HTMLParameter($this->HTMLBuilder, "content\\mobile-category.html");
+
+        $HTMLCategoriesDes = array(); /*template for desktop categories*/
+        $HTMLCategoriesMob = array(); /*template for mobile categories*/
+        $categoryname = array(); /*array for the main category names*/
+
+        $databaseHelper = new \src\classes\DatabaseHelper();
+        $rubric = new \src\classes\Models\Rubric($databaseHelper, 1);
+        $mainCategories = $rubric->getChildren();
+
+        foreach($mainCategories as $category){
+            $categoryname[] = $category->getName();
+        }
+
+        /** @var $HTMLCategoriesDes HTMLParameter[] */
+        for($i = 0; $i < sizeof($categoryname); $i++) { /*adding the main category names to the template for desktop*/
+            $HTMLCategoriesDes[$i] = new HTMLParameter($this->HTMLBuilder, "content\\desktop-category.html");
+            $HTMLCategoriesDes[$i]->addTemplateParameterByString("name", $categoryname[$i]);
+        }
+
+        $toRemove = array(" ", ",", "'");/* an array containing the characters that should be removed for the target of the buttons*/
+
+        /** @var $HTMLCategoriesMob HTMLParameter[] */
+        for($i = 0; $i < sizeof($categoryname); $i++) { /*adding the main category names to the template for mobile*/
+            $HTMLCategoriesMob[$i] = new HTMLParameter($this->HTMLBuilder, "content\\mobile-category.html");
+            $HTMLCategoriesMob[$i]->addTemplateParameterByString("name", $categoryname[$i]);
+            $HTMLCategoriesMob[$i]->addTemplateParameterByString("target-main-category", str_replace($toRemove, "",$categoryname[$i]));/*removing the special characters from the target using the earlier declared array*/
+            $HTMLCategoriesMob[$i]->addTemplateParameterByString("name", $categoryname[$i]);
+            foreach($mainCategories as $maincategory){
+                $a = $maincategory->getChildren();
+                foreach($a as $b) {
+                    $HTMLCategoriesMob[$i]->addTemplateParameterByString("child-categories", $b->getName());
+                }
+            }
+        }
+
+        /*inserting the names into the template*/
+        $HTMLCategoryDesktop->addTemplateParameterByString("name", $this->HTMLBuilder->joinHTMLParameters($HTMLCategoriesDes));
+        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("desktop-category", $HTMLCategoryDesktop);
+        $HTMLCategoryMobile->addTemplateParameterByString("name", $this->HTMLBuilder->joinHTMLParameters($HTMLCategoriesMob));
+        $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("mobile-category", $HTMLCategoryMobile);
+
         echo $this->HTMLBuilder->getHTML();
     }
 }
