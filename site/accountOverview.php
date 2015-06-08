d0c21010518126ab7abbd8fb32e62acaf4413361
Marijn <marijn.martin@hotmail.com>
2015-06-08 21:09:03 +0200

    -push om te zien hoe ontvangen/gegeven feedback nu werkt

--- site/accountOverview.php
+++ site/accountOverview.php
@@ -4,34 +4,28 @@ use src\classes\HTMLBuilder;
 use src\classes\HTMLBuilder\HTMLParameter;
 use src\classes\Models\User;
 use src\classes\Page;
-
 require_once "src/libraries/password.php"; //For password hashing functionality for PHP < 5.5, server is 5.4.35, source: https://github.com/ircmaxell/password_compat
-
 function __autoload($class_name) { //PHP will use this function if a class file hasn't been read yet.
     require $class_name . '.php';
 }
-
 session_start();
 date_default_timezone_set("Europe/Amsterdam");
-
 class accountoverview extends Page
 {
-
     public function __construct()
     {
         parent::__construct("template.html");
     }
-
     public function createHTML()
     {
         $this->user = new User($this->databaseHelper, $_GET["user"]);
-
         $content = new HTMLParameter($this->HTMLBuilder, "content\\content-account-overview.html");
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("content", $content);
         $feedbackTemplate = new HTMLParameter($this->HTMLBuilder, "content\\feedback\\feedback-template.html");
 
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("positive-feedback", $feedbackTemplate);
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByParameter("negative-feedback", $feedbackTemplate);
+
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByString("username", $this->user->getUsername());
         $this->HTMLBuilder->mainHTMLParameter->addTemplateParameterByString("country", $this->user->getCountry());
 
@@ -47,36 +41,28 @@ class accountoverview extends Page
         $this->generateLoginAndRegisterTemplates();
         return $this->HTMLBuilder->getHTML();
     }
-
-
     public function __destruct()
     {
         parent::__destruct();
     }
-
-
-    public function createReceivedFeedbackTemplate($feedbackKind)
-    {
+    public function createReceivedFeedbackTemplate($feedbackKind){
         $feedbackTemplates = array();
         foreach ($feedbackKind as $feedback) {
-            if ($feedback->getUser()->getUsername() !== $this->user->getUsername()) {
+            if ($feedback->getUser() === $this->user) {
                 $feedbackTemplates[] = $this->generateFeedbackTemplate($feedback);
             }
         }
         return $feedbackTemplates;
     }
-
-    public function createGivenFeedbackTemplate($feedbackKind)
-    {
+    public function createGivenFeedbackTemplate($feedbackKind){
         $feedbackTemplates = array();
         foreach ($feedbackKind as $feedback) {
-            if ($feedback->getUser()->getUsername() === $this->user->getUsername()) {
+            if ($feedback->getUser() !== $this->user) {
                 $feedbackTemplates[] = $this->generateFeedbackTemplate($feedback);
             }
         }
         return $feedbackTemplates;
     }
-
     private function generateFeedbackTemplate($feedback)
     {
         $feedbackTemplate = new HTMLParameter($this->HTMLBuilder, "content\\feedback\\feedback-template.html");
@@ -91,10 +77,8 @@ class accountoverview extends Page
         } else {
             $feedbackTemplate->addTemplateParameterByString("feedback-type", '<text style="color: red">Negatief</text>');
         }
-
         return $feedbackTemplate;
     }
 }
-
 $page = new accountOverview();
 echo $page->createHTML();
